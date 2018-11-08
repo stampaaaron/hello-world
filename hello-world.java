efea90bd597466940e1595a5a175f0b54472ac3c
Damien Flury <damien.flury@gmail.com>
2018-11-08 15:17:27 +0100

    Add constructor(string)

--- hello-world.java
+++ hello-world.java
@@ -1,12 +1,16 @@
 public class HelloWorld {
 	private int i;
+
 	public HelloWorld() {
 		i = 0;
 	}
-	
+
+	public HelloWorld(String message) {
+		System.out.println(message);
+	}
+
 	public int getI() {
 		return this.i;
-	
 	}
 
 	public void setI(int i) {
